42b8181b31ad6f817509064775a789a5f031955b
pranav <pranavchoudhary500@gmail.com>
2019-05-22 00:52:40 +0530

    Lifestyle Store:A shopping website

---
+++ LifestyleStore/connection.php
@@ -0,0 +1,5 @@
+<?php
+$con=mysqli_connect("localhost","root","","lifestylestore") or die(mysqli_error($con));
+//$con=mysqli_connect("localhost","root","","store") or die(mysqli_error($con));
+
+?>
